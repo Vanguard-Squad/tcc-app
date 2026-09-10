f9aa7f6de4c4432e6d75b31a0e89b102f9ce0334
itsuki <alexconstantino2909@gmail.com>
2026-09-10 12:02:40 -0300

    spec: adiciona testes de frota
    Cobre o model Vehicle (route/seats_busy/is_active no estado inicial,
    unicidade de placa, validacao de assentos) e o controller (permissao
    por papel, criacao sem rota).

--- test/models/vehicle_test.rb
+++ test/models/vehicle_test.rb
@@ -1,7 +1,33 @@
 require "test_helper"
 
 class VehicleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid fixture" do
+    assert vehicles(:one).valid?
+  end
+
+  test "starts without a route and with no seats occupied" do
+    _owner, company = create_company_with_owner!
+    vehicle = create_vehicle!(company: company)
+
+    assert_nil vehicle.route
+    assert vehicle.is_active?
+    assert_equal 0, vehicle.seats_busy
+  end
+
+  test "requires a unique license_plate" do
+    _owner, company = create_company_with_owner!
+    vehicle = create_vehicle!(company: company)
+
+    duplicate = Vehicle.new(company: company, license_plate: vehicle.license_plate, seats: 10)
+    assert_not duplicate.valid?
+    assert_includes duplicate.errors[:license_plate], "já está em uso"
+  end
+
+  test "requires a positive integer number of seats" do
+    _owner, company = create_company_with_owner!
+
+    vehicle = Vehicle.new(company: company, license_plate: "XYZ-0001", seats: 0)
+    assert_not vehicle.valid?
+    assert_includes vehicle.errors[:seats], "precisa ser maior que 0"
+  end
 end
