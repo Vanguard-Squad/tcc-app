ff21da3509fe755a47c39b4f33b847f81a560169
itsuki <alexconstantino2909@gmail.com>
2026-09-10 11:05:24 -0300

    chore: configura locale pt-BR e fuso horario
    Mensagens de validacao (Rails/ActiveRecord) passam a sair em portugues
    em vez de ingles, consistente com o resto da interface. Fuso horario
    ajustado para America/Sao_Paulo.

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,8 @@ module TccApp
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "America/Sao_Paulo"
+    config.i18n.default_locale = :"pt-BR"
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
